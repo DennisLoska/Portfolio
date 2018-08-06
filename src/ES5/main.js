17afee7ca27f83e52672c823ab3bcb2275656be5
dennis <dennis.loska@firstfinance-gmbh.de>
2018-08-06 11:48:49 +0200

    finishing navigation for now

--- src/ES5/main.js
+++ src/ES5/main.js
@@ -1,1 +1,5 @@
 "use strict";
+
+function showNavigation() {
+    $('#nav-list').toggleClass("hide-navbar");
+}
---
+++ src/js/main.js
@@ -0,0 +1,3 @@
+function showNavigation() {
+    $('#nav-list').toggleClass("hide-navbar")
+}
